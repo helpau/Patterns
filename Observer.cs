6023d1e6598d4738807f95d145f360d67d783a48
Adam Smith <maxmakskanal@outlook.com>
2022-06-20 19:29:28 +0500

    Добавьте файлы проекта.

---
+++ Observer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns
+{
+    class Observer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Observer!");
+        }
+    }
+}
---
+++ Strategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns
+{
+    class Strategy
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Strategy!");
+        }
+    }
+}
