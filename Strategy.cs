ae91749e92c1c4f1078cad7a08aaa8058d64c51d
Adam Smith <maxmakskanal@outlook.com>
2022-06-20 20:51:37 +0500

    feat: row variant bridge with trees classes
    need fil Main()

--- Strategy.cs
+++ Strategy.cs
@@ -6,7 +6,68 @@ namespace Patterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strategy!");
+            Yunga yunga = new Yunga();
         }
     }
+
+
+
+    class Pirate
+    {
+        int health = 100;
+
+        ISmokeBehavior smokeBehavior;
+        public ISmokeBehavior SmokeBehavior
+        { 
+            set => smokeBehavior = value;
+            get => smokeBehavior;
+        }
+
+    }
+
+    class Yunga : Pirate
+    {
+
+    }
+
+    class Botson : Pirate
+    {
+
+    }
+    class Captain : Pirate { }
+    interface ISmokeBehavior
+    {
+        void Smoke();
+    }
+    interface IDrinkBehavior
+    {
+        void Drink();
+    }
+    interface IShootBehavior
+    {
+        void Shoot();
+    }
+    class SmokeTabacco : ISmokeBehavior
+    {
+        public void Smoke()
+        {
+            Console.WriteLine("Курим табак");
+        }
+    }
+    class SmokeSnuff : ISmokeBehavior
+    {
+        public void Smoke()
+        {
+            Console.WriteLine("Курим махорку");
+        }
+    }
+
+    class NoSmoke : ISmokeBehavior
+    {
+        public void Smoke()
+        {
+            Console.WriteLine("Не курим");
+        }
+    }
+
 }
